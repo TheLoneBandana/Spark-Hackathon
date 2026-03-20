101f1686705df3056fcdcabc6d54c7092f4668b6
Sidney-Land <sidneyaland@gmail.com>
2026-03-20 11:01:54 -0400

    Enemy Movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Rigidbody2D enemyRB;
+    public float enemySpeed = .05f;
+    public float horizontalWidth = 5f;
+
+    public GameObject bulletPrefab;
+    public int shootDelay = 2;
+
+    private Vector2 shootDirection = Vector2.down;
+    private float lifetime = 0;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        enemyRB = gameObject.GetComponent<Rigidbody2D>();
+
+        lifetime = 0;
+
+        if (bulletPrefab != null)
+        {
+            StartCoroutine(Shoot());
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        lifetime += enemySpeed * Time.fixedDeltaTime;
+
+        enemyRB.position = new Vector2(horizontalWidth * Mathf.Sin(lifetime), -lifetime);
+    }
+
+    IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(shootDelay);
+        GameObject bullet = Instantiate(bulletPrefab);
+        bullet.GetComponent<Rigidbody2D>().linearVelocity = shootDirection;
+    }
+}
