8523544bd9a4d29f840c327e44a54ad358b6021f
Dianming Chang <changjohnny983@gmail.com>
2026-03-20 15:36:43 -0400

    Add projectile max range and speed tracking
    Introduce a max flight range and track projectile travel distance. Adds MAX_FLY_DISTANCE constant plus public speed and currentFlyDistance fields. Update() now calls UpdateFlyDistance(), which increments distance by Time.deltaTime * speed and destroys the projectile when it exceeds the max range. initializeProj() stores the passed speed on the instance so distance updates work correctly.

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour
 {
+    public const float MAX_FLY_DISTANCE = 10F;
+
     public Sprite playerProjSprite; 
     public Sprite enemyProjSprite;
 
     public bool isPlayerProjectile;
     //used in case we want enemies to have different levels of damage
     public int enemyDamage;
+    public float speed;
+    public float currentFlyDistance;
 
     private Rigidbody2D projRB;
 
@@ -36,7 +40,16 @@ public class ProjectileScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateFlyDistance();
+    }
+
+    private void UpdateFlyDistance()
+    {
+        currentFlyDistance += UnityEngine.Time.deltaTime * speed;
+        if(currentFlyDistance >= MAX_FLY_DISTANCE)
+        {
+            UnityEngine.Object.Destroy(this.gameObject);
+        }
     }
 
     public void initializeProj(int damage, bool isPlayerProj, Vector2 direction, float speed)
@@ -44,6 +57,7 @@ public class ProjectileScript : MonoBehaviour
         enemyDamage = damage;
         isPlayerProjectile = isPlayerProj;
         projRB.linearVelocity = speed * direction;
+        this.speed = speed;
 
         gameObject.GetComponent<SpriteRenderer>().sprite = isPlayerProjectile ? playerProjSprite : enemyProjSprite;
     }
